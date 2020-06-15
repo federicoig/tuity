69a0ec6208c93210ab825b5dd5b187eab87cc0f5
federicoig <federicoignacio1@gmail.com>
2020-06-14 22:26:28 -0300

    Updates

---
+++ precache-manifest.a956336512eb2816cdf14fe9beb4ffe7.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7839c39f0b057defe168088d74fb5d",
+    "url": "/tuity/index.html"
+  },
+  {
+    "revision": "49f4cd8a769e0ab69542",
+    "url": "/tuity/static/css/main.54204d01.chunk.css"
+  },
+  {
+    "revision": "54db52d5b70a5ee6078f",
+    "url": "/tuity/static/js/2.5367e7d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tuity/static/js/2.5367e7d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f4cd8a769e0ab69542",
+    "url": "/tuity/static/js/main.fea624d6.chunk.js"
+  },
+  {
+    "revision": "f57e22673128041faf47",
+    "url": "/tuity/static/js/runtime-main.8b5e5423.js"
+  },
+  {
+    "revision": "d1d4dfa494ac0224ab612758e9458ad5",
+    "url": "/tuity/static/media/bell.d1d4dfa4.svg"
+  },
+  {
+    "revision": "6f66df60cae8db23cc7c2eea78b80b1a",
+    "url": "/tuity/static/media/bookmarks.6f66df60.svg"
+  },
+  {
+    "revision": "15cf82554cc426bf0e2ac64bc0751ccb",
+    "url": "/tuity/static/media/comment.15cf8255.svg"
+  },
+  {
+    "revision": "df6538c68aa22eb842c418e3f484e834",
+    "url": "/tuity/static/media/emoji.df6538c6.svg"
+  },
+  {
+    "revision": "9172f7280927d05f8aae5150128f126a",
+    "url": "/tuity/static/media/gallery.9172f728.svg"
+  },
+  {
+    "revision": "96385fcb8a69636bce0c98b3fffdc81f",
+    "url": "/tuity/static/media/gif.96385fcb.svg"
+  },
+  {
+    "revision": "fb79059e1a1b780f84300cc270f65021",
+    "url": "/tuity/static/media/hashtag.fb79059e.svg"
+  },
+  {
+    "revision": "81c84dc744e1151cf7c85ce40a78ff20",
+    "url": "/tuity/static/media/home.81c84dc7.svg"
+  },
+  {
+    "revision": "69d27f81e553bd5b1f940c6714ddff44",
+    "url": "/tuity/static/media/like.69d27f81.svg"
+  },
+  {
+    "revision": "92a1565c367ce81fcc20f807767409ba",
+    "url": "/tuity/static/media/lists.92a1565c.svg"
+  },
+  {
+    "revision": "0c3279495bce51685902a40dc8eec578",
+    "url": "/tuity/static/media/logo.0c327949.svg"
+  },
+  {
+    "revision": "a9e228fe485035ee286349dd0d958388",
+    "url": "/tuity/static/media/message.a9e228fe.svg"
+  },
+  {
+    "revision": "11e4d95f248290c904652bb57d84c314",
+    "url": "/tuity/static/media/options.11e4d95f.svg"
+  },
+  {
+    "revision": "f4d1e6cf5de1686c3f7fe26f333978d2",
+    "url": "/tuity/static/media/poll.f4d1e6cf.svg"
+  },
+  {
+    "revision": "f078c0a3d6e8004445d9c86e5cfbe534",
+    "url": "/tuity/static/media/profile.f078c0a3.svg"
+  },
+  {
+    "revision": "2252aa2dd3da494a5b3159ac8703b96e",
+    "url": "/tuity/static/media/retuit.2252aa2d.svg"
+  },
+  {
+    "revision": "c913462a9ee55e13830dbcb2c9ee5742",
+    "url": "/tuity/static/media/schedule.c913462a.svg"
+  },
+  {
+    "revision": "d1b561bd7052d6e97992c9ab1e4ff50e",
+    "url": "/tuity/static/media/search.d1b561bd.svg"
+  },
+  {
+    "revision": "f98a8d33bce48e6ae6b0fc37c58f1058",
+    "url": "/tuity/static/media/share.f98a8d33.svg"
+  },
+  {
+    "revision": "df7de5c6800cd28aff8a57e2e8bab32f",
+    "url": "/tuity/static/media/sparks.df7de5c6.svg"
+  }
+]);
